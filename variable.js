3b049a55d3b4b7d39e1e48f756a67fed6682e96b
punit-0522 <punitjaiswal785621@gmail.com>
2026-04-26 22:08:05 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,27 @@
+// // variable are just like containers to store the data
+// // *** VAR:variable can be re-declared & updated.A global scope variable----------------
+
+// var Number=55;
+// var strings ="a";
+// var Number = 33; //(re-declared & updated too)
+// console.log(Number);
+// console.log(strings);
+
+//*** LET:variable cannot be re-declared but can be updated.A block scope variable----------------
+
+// let num =10;
+// let strings='learning';
+// num= 30; //(updated but not re-decleared)
+// console.log(num);
+// console.log(strings);
+
+// *** CONST:variable cannot be re-decleared or updated.A block scope variable
+
+const num = 10;
+const strings = 'JavaScript'; //( cannot be re-decleared or updated)
+console.log(num);
+console.log(strings);
+
+//*** typeof_operator :used to check its data type
+console.log(typeof(num));
+console.log(typeof(strings));
