333099e4bbab78e1ff92928cc8ddfe8b2da76dd2
punit-0522 <punitjaiswal785621@gmail.com>
2026-04-27 21:13:29 +0530

    Update Operartors.js

--- Operartors.js
+++ Operartors.js
@@ -1,4 +1,5 @@
 // // *** Arithmetic_Operators-------------
+
 // let a = 120;
 // let b = 30;
 // console.log(a+b);
@@ -7,7 +8,10 @@
 // console.log(a/b);
 // console.log(a%b);
 
+
+
 // // *** Assignment_Operators-----------------
+
 // let a=3;
 // let b=5;
 // a+=b;//a=a+b
@@ -18,22 +22,34 @@
 // a**=b;
 // console.log(a)
 
+
+
 // // *** Comparison Operatoes------------------
+
 // let a=3; //number
 // let b='3'; //string
 // console.log(a===b)
 
+
+
 // //*** Logical Operators------------------
+
 // let a=1;
 // let b=0;
 // console.log(a&&b)
 
+
+
 // // *** Ternary Operator--------------
+
 // let age=35;
 // let result=(age>18)?"can vote" :"cannot vote";
 // console.log(result)
 
+
+
 //*** Template literal (back tick)---------
+
 let name = "'Punit Jaiswal'";
 let id = "Et22bthcs104";
 let num = 7577820021;
