660ddc81b22d49a7477cca49d262da826020267a
punit-0522 <punitjaiswal785621@gmail.com>
2026-04-27 21:12:27 +0530

    Add files via upload

---
+++ Operartors.js
@@ -0,0 +1,40 @@
+// // *** Arithmetic_Operators-------------
+// let a = 120;
+// let b = 30;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// // *** Assignment_Operators-----------------
+// let a=3;
+// let b=5;
+// a+=b;//a=a+b
+// a=b;
+// a-=b;
+// a*=b;
+// a%=b;
+// a**=b;
+// console.log(a)
+
+// // *** Comparison Operatoes------------------
+// let a=3; //number
+// let b='3'; //string
+// console.log(a===b)
+
+// //*** Logical Operators------------------
+// let a=1;
+// let b=0;
+// console.log(a&&b)
+
+// // *** Ternary Operator--------------
+// let age=35;
+// let result=(age>18)?"can vote" :"cannot vote";
+// console.log(result)
+
+//*** Template literal (back tick)---------
+let name = "'Punit Jaiswal'";
+let id = "Et22bthcs104";
+let num = 7577820021;
+console.log(`Hello, my name is ${name} and my id is ${id} and my ph no. is ${num}`)
