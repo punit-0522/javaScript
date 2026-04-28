78cf21bc45c6484503b1c9329b1c5da327591fd7
punit-0522 <punitjaiswal785621@gmail.com>
2026-04-28 23:23:40 +0530

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,45 @@
+// //*** If Statements------------ 
+// let age =18
+// if(age>=18){
+//     console.log("can vote")
+// }
+
+// //*** if-else Statement----------
+// let age =2;
+// if(age>+18){
+//     console.log("can drive")
+// }else{
+//     console.log("cannot drive")
+// }
+
+// // *** else-if Statement--------------
+// let age = 0;
+// if (age >= 1 && age <= 5) {
+//     console.log("Kids")
+// } else if (age > 5 && age <= 18) {
+//     console.log("Teenagers")
+// } else if(age > 18) {
+//     console.log("Adults")
+// } else {
+//     console.log("invalid input")
+// }
+
+// *** Switch Statements---------------
+let day=8;
+switch(day){
+    case 1: console.log("sunday");
+    break;
+     case 2: console.log("monday");
+    break;
+     case 3: console.log("tuesday");
+    break;
+     case 4: console.log("wednesday");
+    break;
+     case 5: console.log("thursday");
+    break;
+     case 6: console.log("friday");
+    break;
+     case 7: console.log("saturday");
+    break;
+    default:console.log("Invalid input")
+}
